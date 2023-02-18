0e01fd02b8fdde621d9d600e07fe05787facf9d1
Matthew Ashman <matthewiashman@gmail.com>
2023-02-18 18:37:00 +0000

    Update dinner guests as someone cannot make it

--- 3/dinner_guests.py
+++ 3/dinner_guests.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+import random
+
+
 guests = [
     "Archimedes",
     "Jesus",
@@ -9,3 +12,8 @@ guests = [
 
 for guest in sorted(guests):
     print(f"I would like {guest} to come to dinner")
+
+busy = random.choice(guests)
+guests.remove(busy)
+
+print(f"Sadly, {busy} cannot make it :(")
