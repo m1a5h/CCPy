f7c1ecc28acf72f0d75240832d4624f7f100b737
Matthew Ashman <matthewiashman@gmail.com>
2023-02-18 18:33:43 +0000

    Invite famous guests to dinner

---
+++ 3/dinner_guests.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+guests = [
+    "Archimedes",
+    "Jesus",
+    "Grothendieck",
+    "Trotsky"
+    ]
+
+for guest in sorted(guests):
+    print(f"I would like {guest} to come to dinner")
